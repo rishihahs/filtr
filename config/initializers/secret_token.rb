14bec01da9a6bf4262b4fe6d15ae4fe42f5c0256
Rishi Shah <rishihahs@gmail.com>
2011-07-04 23:17:49 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Filtr::Application.config.secret_token = 'cdb8b62783bf0c53fde0b5a1e410c3851506fa99f112c300fb36ca0407f6905e180c9ad3c0548c0911fab4ebc02953d73c859d1339f047c5a0ba664dc6a3b8e9'
